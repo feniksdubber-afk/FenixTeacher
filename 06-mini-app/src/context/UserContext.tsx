6389f90f20e9079ef413fd954c802d0d6a72b985
feniksdubber-afk <feniksdubber@gmail.com>
2026-09-18 13:54:34 +0500

    Add files via upload

--- 06-mini-app/src/context/UserContext.tsx
+++ 06-mini-app/src/context/UserContext.tsx
@@ -1,1 +1,8 @@
+import { createContext, useContext } from "react";
+import { FenixUser } from "../api/fenix";
 
+export const UserContext = createContext<FenixUser | null>(null);
+
+export function useFenixUser(): FenixUser | null {
+  return useContext(UserContext);
+}
